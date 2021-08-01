2ea938919990f2d235852d257225a9d3f73c981f
rob mcglade <rmcglade@miesh.io>
2021-08-01 21:25:58 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,14 @@ import { projectIcons } from '../components/Icons';
 import React, { useState, useEffect } from 'react';
 
 import { projects } from '../utils/projectsData';
+
 const [data, setData] = useState('');
 const Home = () => (
   <div className="home">
     <h1>What Can I Deploy to Static Apps?</h1>
     <div className="card-grid">
       {projects.map((project) => {
-        useEffect();
+     
         const Icon = projectIcons[project.id];
     
         return <SmallCard key={project.id} Icon={Icon} title={project.name +' '+ data} slug={project.slug} />;
@@ -22,7 +23,7 @@ const Home = () => (
 
 
 
-  const useEffect=(() => {
+  useEffect=(() => {
     (async function () {
       const { text } = await( await fetch(`/api/message`)).json();
       setData(text);
